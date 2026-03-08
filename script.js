3fdcbd13511fe5dcb1332ce1d66eec4e7c40ca68
Rbatista06 <rbatistap.0610@gmail.com>
2026-03-08 08:28:25 -0300

    scriptjss

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll("nav a").forEach(link => {
+
+link.addEventListener("click", function(e){
+
+e.preventDefault()
+
+const target = document.querySelector(this.getAttribute("href"))
+
+target.scrollIntoView({
+behavior:"smooth"
+})
+
+})
+
+})
+
+document.querySelectorAll(".galeria img").forEach(img => {
+
+img.addEventListener("click", () => {
+
+window.open(img.src)
+
+})
+
+})
