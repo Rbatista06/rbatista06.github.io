c850a5db98a1d47e97f5d1dbddb740edaf29ab2d
Rafael Batista Prescinato <rbatistap.0610@gmail.com>
2026-03-08 09:51:42 -0300

    ...

--- script.js
+++ script.js
@@ -23,3 +23,57 @@ window.open(img.src)
 })
 
 })
+
+//responsivo.
+@media (max-width: 900px){
+
+header{
+flex-direction:column;
+gap:15px;
+text-align:center;
+}
+
+nav a{
+margin:0 10px;
+}
+
+.hero h2{
+font-size:36px;
+}
+
+section{
+padding:60px 30px;
+}
+
+//projeto destaque
+
+.projeto{
+flex-direction:column;
+text-align:center;
+}
+
+.projeto-img{
+width:100%;
+max-width:300px;
+height:auto;
+}
+
+/* galeria */
+
+.galeria{
+justify-content:center;
+flex-wrap:wrap;
+}
+
+.galeria img{
+height:140px;
+}
+
+//contato
+
+.contato-links{
+flex-direction:column;
+align-items:center;
+}
+
+}
